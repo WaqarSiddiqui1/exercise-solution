dba7c4c8d1aeba6db771fa9abafcfbcc9de3b3be
Waqar <mwsiddiqui15@gmail.com>
2022-07-07 17:13:48 +0100

    Solution 2 Added.

---
+++ solution2.js
@@ -0,0 +1,9 @@
+const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'Peter' ];
+
+
+
+for (let i = 0; i < teachingTeam.length; i++) {
+    teachingTeam[i] = teachingTeam[i].toUpperCase();
+} 
+
+console.log(teachingTeam);
