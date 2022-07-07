5425aa0edf07d9e64ce383acd892c13e150bc446
Waqar <mwsiddiqui15@gmail.com>
2022-07-07 17:02:42 +0100

    Add solution to question 1 of the Exercise.
    This is a multi-line commit message for
    our test.

---
+++ solution1.js
@@ -0,0 +1,8 @@
+const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'Peter' ];
+
+
+for (member of teachingTeam) {
+    if (member.includes('o')) {
+        console.log(member);
+    }
+}
